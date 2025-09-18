1740820ada662135fe0796fa1393a1ef55497e51
Artem <WWW.Woks@outlook.com>
2025-09-18 15:06:05 +0700

    Added a base template for index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>лента</title>
+</head>
+<body>
+    <header>
+        <h1>Tipix</h1>
+    </header>
+    <aside>
+
+    </aside>
+    <main>
+        <p>Ой тут пусто</p>
+    </main>
+    <footer>
+        <p>&copy; Все права защищены</p>
+    </footer>
+</body>
+</html>
